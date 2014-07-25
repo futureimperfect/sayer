1ee7cb7308710e6419690f33e09bf3e0d3fbecab
futureimperfect <james@everythingisgray.com>
2014-07-25 01:10:59 -0700

    Added functionality to say the words in the text field in reverse.

---
+++ Sayer/EIGStringReverser.h
@@ -0,0 +1,13 @@
+//
+//  EIGStringReverser.h
+//  Sayer
+//
+//  Created by James Barclay on 7/25/14.
+//  Copyright (c) 2014 Everything is Gray. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EIGStringReverser : NSObject
+
+@end
---
+++ Sayer/EIGStringReverser.m
@@ -0,0 +1,13 @@
+//
+//  EIGStringReverser.m
+//  Sayer
+//
+//  Created by James Barclay on 7/25/14.
+//  Copyright (c) 2014 Everything is Gray. All rights reserved.
+//
+
+#import "EIGStringReverser.h"
+
+@implementation EIGStringReverser
+
+@end
